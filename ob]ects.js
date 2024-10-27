90174faff50c5299eb034318ced364c65a92c4b7
Grem.lin <arbudin@gmail.com>
2024-10-28 00:09:28 +0400

    finish objects research

--- ob]ects.js
+++ ob]ects.js
@@ -62,4 +62,48 @@
             obj["display"]();
             obj["бетмен и джокер сражаются"](); // можно использовать любые строки, даже длинные русские названия через пробел.
 
-            
+            // объявление полей и методов через массивы позволяет динамически менять название полей 
+            function create_a_fight(who,enemy){
+                obj = {
+                    [who]: "красавчик",
+                    [enemy] : "падла",
+                
+                    ["fight"] : function(){
+                        console.log (who+"-"+this[who]+" и "+enemy+"-"+this[enemy]+ " сражаются");
+                    }
+                }
+                return obj;
+            }
+            obj = create_a_fight("batman","joker");
+            obj.fight();
+
+            // поля можно динамически удалять. синтаксис через точку или через массив используется слово delete
+            console.clear();
+            obj = {
+                hero:"batman",
+                enemy:"jocker"
+            }
+            console.log(obj);
+            delete obj.hero;
+            delete obj["enemy"];
+            console.log(obj);
+
+            // в конструктор объекта можно передать заранее объявленные переменные со значениями
+            // название переменной станет названием поля азначение перейдет понаследству.
+         
+            hero = "barman";
+            enemy = "joker";
+            fight = function(){ console.log(this)};
+            obj = {hero,enemy,fight};
+            console.log("передать в конструктор объвленные переменные")
+            obj.fight();
+
+            // создать обхект из массива парзначений с помощью Object.fromEntries .(наверное нужно для джейсона?)
+
+            list = [["hero","batman"],["enemy","joker"]];
+            obj = Object.fromEntries(list);
+            console.log("создание объекта с помощь Object.fromEntries");
+            console.log(obj);
+
+
+ 
